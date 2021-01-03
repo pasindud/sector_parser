422ef14c67016157034ab9dae4d053725be3cd7a
Pasindu <ppasindud@gmail.com>
2021-01-03 18:17:15 +0800

    Add support for REGION in sct v2

--- src/region.js
+++ src/region.js
@@ -23,6 +23,12 @@ class Region {
         return JSON.stringify(this, null, 2)
     }
 
+    static filterLine(line) {
+        if (line == undefined) return '';
+
+        return line.replace('\r', '').replace(/  +/g, ' ').trim();
+    }
+
     /**
      * @returns {Object} of Region in GeoJSON format.
      */
@@ -53,8 +59,16 @@ class Region {
      */
     static parse(contents) {
         function _addRegion() {
+            var color = null
+
+            if (version == 2) {
+                color = 0;
+                if (version2Color != null) {
+                    defineColorName = version2Color;
+                }
+            }
+
             if (defineColorName != null) {
-                var color = null
 
                 if (!isNaN(defineColorName)) {
                     color = DefineColor.getHex(defineColorName)
@@ -80,9 +94,14 @@ class Region {
         let currentPoints = []
         // Whether we have started to parse the REGION section.
         let started = false
+        let version = 1;
+        let version2Color = null;
 
-        for (var i = 0; i < content.length; i++) {
-            const line = content[i].replace('\r', '').replace(/  +/g, ' ').trim()
+        let i = -1;
+        while(i < content.length) {
+            ++i;
+
+            const line = Region.filterLine(content[i]);
 
             // Skip comments and empty lines.
             if (line === '' || line.startsWith(';')) {
@@ -96,9 +115,24 @@ class Region {
                 _addRegion()
                 currentPoints = []
                 defineColorName = null
+                version2Color = null;
+                version = 1;
                 // customColor = null
                 started = false
                 continue
+            } else if (Region.filterLine(content[i]).startsWith('REGIONNAME')) {
+                // Finish the previous region.
+                _addRegion()
+                currentPoints = []
+
+                version = 2;
+
+                var segs = Region.filterLine(content[i+1]).split(' ');
+                version2Color = segs[0].trim();
+
+                currentPoints.push(new LatLong(segs[1].trim(), segs[2].trim()))
+                i = i + 1;
+                continue;
             }
 
             if (!started) continue
@@ -108,6 +142,17 @@ class Region {
             if (segs.length <= 1) {
                 continue
             }
+
+
+            if (version === 2) {
+                if (segs.length != 2) {
+                    console.error('Region v2 error ' + line)
+                }
+
+                currentPoints.push(new LatLong(segs[0].trim(), segs[1].trim()))
+                continue;
+            }
+
             if (segs[segs.length - 1].trim().startsWith(';')) {
                 segs = segs.slice(0, segs.length - 1)
             }
@@ -144,7 +189,7 @@ class Region {
             _addRegion()
         }
 
-        return regions
+        return regions;
     }
 }
 
